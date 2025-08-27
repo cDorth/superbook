fbbb548a095ea15cccdceb42e3ea0db9c8ad4112
Dorth <carloseduardodorth123@gmail.com>
2025-08-27 14:50:29 -0300

    criado primeira views post+rota+template

--- superbook/urls.py
+++ superbook/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('heroes/', include('heroes.urls')),  # rotas do app heroes
-    # path('posts/', include('posts.urls')),    # rotas do app posts
+    path('posts/', include('posts.urls')),    # rotas do app posts
 ]
